b27196cea1596d60f7217cb84e9af5196f1c2b4c
Jana-ss <s444007448@uqu.edu.sa>
2026-08-02 17:24:44 +0300

    Update text from 'NUSUQ' to 'NUSUQ - Jenkins Demo'

--- nu/lib/pages/login_page.dart
+++ nu/lib/pages/login_page.dart
@@ -274,7 +274,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       Column(
                         children: [
                           const Text(
-                            'NUSUQ',
+                            'NUSUQ - Jenkins Demo',
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 26,
